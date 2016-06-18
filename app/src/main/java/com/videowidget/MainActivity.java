0b4e61e445ae09d27f29fdedc486af77cead26e6
shahbaz <m.azam.shahbaz@gmail.com>
2016-06-18 14:46:00 +0500

    First commit

---
+++ app/src/main/java/com/videowidget/MainActivity.java
@@ -0,0 +1,78 @@
+package com.videowidget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.res.Configuration;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends Activity {
+
+    private VideoView myVideoView;
+    private int position = 0;
+    private ProgressDialog progressDialog;
+    private MediaController mediaControls;
+    String srcPath = "https://ia800208.us.archive.org/4/items/Popeye_forPresident/Popeye_forPresident_512kb.mp4";
+
+    String TAG = "com.ebookfrenzy.videoplayer";
+    ProgressDialog pDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final VideoView mVideoView = (VideoView) findViewById(R.id.video_view);
+
+        pDialog = new ProgressDialog(this);
+
+        // Set progressbar message
+        pDialog.setMessage("Buffering...");
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(false);
+        // Show progressbar
+        pDialog.show();
+
+        try {
+            // Start the MediaController
+            MediaController mediacontroller = new MediaController(this);
+            mediacontroller.setAnchorView(mVideoView);
+            srcPath = "http://clips.vorwaerts-gmbh.de/VfE_html5.mp4";
+            Uri videoUri = Uri.parse(srcPath);
+            mVideoView.setMediaController(mediacontroller);
+            mVideoView.setVideoURI(videoUri);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+        mVideoView.requestFocus();
+        mVideoView.setOnPreparedListener(new OnPreparedListener() {
+            // Close the progress bar and play the video
+            public void onPrepared(MediaPlayer mp) {
+                pDialog.dismiss();
+                mVideoView.start();
+            }
+        });
+        mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            public void onCompletion(MediaPlayer mp) {
+                if (pDialog.isShowing()) {
+                    pDialog.dismiss();
+                }
+                finish();
+            }
+        });
+    }
+
+
+}
